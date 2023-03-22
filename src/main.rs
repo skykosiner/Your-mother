245e34c70f97aa03f076afae87e42f7cc9ccdbcd
Sky Kosiner <sky@skykosiner.co.uk>
2023-03-22 13:42:32 +0000

    feat: Update to the great FOSS software

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ impl YourMom {
     fn get_weight(&self) {
         println!("Your mother's weight is {}lb", self.weight)
     }
+
+    fn deez_nuts(&self) {
+        println!("Your mother seems to be so fat, that the only thing that can get into her is deez nuts")
+    }
 }
 
 fn main() {
     let your_mom = YourMom { weight: 69.420 };
     your_mom.get_weight();
+    your_mom.deez_nuts();
 }
